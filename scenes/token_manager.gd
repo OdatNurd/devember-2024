256f3c98293f093ea04bcd01486ba905950d2e9e
Terence Martin <odatnurd@gmail.com>
2024-12-04 21:23:05 -0600

    Fix the active token handling in the manager
    After a bit of head scratching revolving around the ordering of events
    that we get being not what we expect, as well as a small logic bomb in
    the code, this fixes the token manager so that it can properly track
    which token is the currently active one, if any.

--- scenes/token_manager.gd
+++ scenes/token_manager.gd
@@ -36,38 +36,47 @@ func _ready() -> void:
 
 ## -----------------------------------------------------------------------------
 
-
-# This is connected to all game tokens and allows them to signal us when the
-# mouse enters or exits their area.
+# This is connected to the mouse event signals of all tokens in the "tokens"
+# group in _ready(); every time a token has the mouse enter or exit it, we get
+# invoked, so that we can track the state of which token is active.
+#
+# Note that due to the vagaries of signals, if the mouse is moving fast enough
+# to enter and exit multiple tokens in quick succession, it is possible for the
+# events to arrive to tell you about things in an order that you don't expect
+# (such as two enters followed by two leaves). So this needs to take care.
 func _mouse_state_change(token: Node, entered: bool) -> void:
-    print("mouse state change:", token.get_path(), " ", entered)
     var new_active = null
     var old_active = null
 
-    # If this is an enter, then a token is gaining the activation status.
+    # When the mouse enters a token, that token always gains the activation
+    # status, possibly from an already active token.
     if entered:
         new_active = token
         old_active = _active_token
 
-    # If this an exit, then a token is being deactivated, and that token must
-    # be the one that is currently active.
+    # When the mouse is exiting a token, that token is deactivated, but only if
+    # it is currently active. Otherwise we ignore the event because it might be
+    # coming out of order.
     else:
-        old_active = _active_token
+        if _active_token == token:
+            old_active = _active_token
+        else:
+            # Exit because the code below will change the active token to null,
+            # which might not be what we want.
+            return
 
-    # If there is an old active token, that one is no longer going to be
-    # active, so deactivate it.
+    # If there used to be something active that now is not, deactivate it.
     if old_active != null:
-        print("deactivating token: ", old_active.get_path())
-        token_deactivated.emit(old_active)
         old_active.deactivate()
+        token_deactivated.emit(old_active)
 
-    # Set the active token to be the newly activated token; if there is actually
-    # a token to be active, then emit the signal as well.
+    # Set the newly activated token, and if required signal people to tell them
+    # about the change. This also handles the fall through from the exited state
+    # to remove the active token.
     _active_token = new_active
     if new_active != null:
-        print("activating token: ", _active_token.get_path())
-        token_activated.emit(_active_token)
         _active_token.activate()
+        token_activated.emit(_active_token)
 
 
 ## -----------------------------------------------------------------------------
