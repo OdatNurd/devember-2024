642a20e90ff1710428f70e2e43091d05581bd02c
Terence Martin <odatnurd@gmail.com>
2024-12-14 23:18:56 -0600

    Start the nominal deck loading code
    This commit is the start of our attempt to dynamically create a deck of
    cards based on having the scene that is associated with them and the
    resources that we set up in the prior commit.
    
    Sadly before we got too far along the route we got confused about why
    the card was not reporting the correct name in the initial debug logic
    only to find that for some reason any event card created, regardless of
    how it appears in the editor, seems to want to all be the same as the
    first event card in the main scene.
    
    We did not get to the bottom of this mystery before we ran out of stream
    time so this will have to be tackled in Day 15 at some point.

--- scenes/components/card_deck.gd
+++ scenes/components/card_deck.gd
@@ -10,3 +10,53 @@ class_name CardDeck extends BaseToken
 
 
 ## -----------------------------------------------------------------------------
+
+
+@export_group("Deck Details", "deck")
+
+## The cards that should be populated into this deck.
+@export var deck_cards : DeckLayout
+
+
+## -----------------------------------------------------------------------------
+
+
+func _ready() -> void:
+    super._ready()
+    _load_cards()
+
+
+
+## -----------------------------------------------------------------------------
+
+
+# Using our configured resource, create all of the required cards in the deck.
+#
+# 1. Get the list of card details from the deck layout
+# 2. For each card in the deck details:
+#    b. Create an instance of the card details
+#    c. Configure the base properties of the instance
+#    d. Configure the token and detail resources for the card
+#
+# None of these resources will be added to the tree, hence they will not
+# actually be visible anyway until they are "dealt".
+func _load_cards() -> void:
+    if deck_cards == null:
+        print("cannot load cards; no deck defined")
+        return
+
+    print("Creating %d cards for %s" % [len(deck_cards.cards), token_details.name])
+
+    var card := deck_cards.card_scene
+    for item in deck_cards.cards:
+        print("Adding card %s" % item.token.name)
+        #var new_card = card.instantiate()
+        #new_card.token_details = item.token
+        #new_card.card_details = item.card
+        #new_card.position = Vector2(300, 300)
+        #get_parent().add_child(new_card)
+
+
+
+
+## -----------------------------------------------------------------------------
