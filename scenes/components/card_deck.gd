2488bb01eb2f0f8c12ac749705a555e356ca28c8
Terence Martin <odatnurd@gmail.com>
2024-12-16 22:10:58 -0600

    Add actual card creation to decks
    This is still a work in progress pending some other structural changes
    that need to happen, but this is literally all of the code that is
    needed to get cards created.
    
    Currently they are not visualized, and the current design idea is to
    have decks associate with a specific token (as yet uncreated CardPile)
    that indicates where cards should be placed when they are drawn.
    
    The newly created cards will take their size and position cues from the
    card pile.
    
    If there is no card pile (such as right now), the cards will be created
    using the deck as the card pile instead.

--- scenes/components/card_deck.gd
+++ scenes/components/card_deck.gd
@@ -17,6 +17,8 @@ class_name CardDeck extends BaseToken
 ## The cards that should be populated into this deck.
 @export var deck_cards : DeckLayout
 
+## The list of cards that this deck contains.
+var _cards : Array[BaseCard]
 
 ## -----------------------------------------------------------------------------
 
@@ -25,6 +27,7 @@ func _ready() -> void:
     super._ready()
     _load_cards()
 
+    _cards = []
 
 
 ## -----------------------------------------------------------------------------
@@ -32,14 +35,17 @@ func _ready() -> void:
 
 # Using our configured resource, create all of the required cards in the deck.
 #
-# 1. Get the list of card details from the deck layout
-# 2. For each card in the deck details:
-#    b. Create an instance of the card details
-#    c. Configure the base properties of the instance
-#    d. Configure the token and detail resources for the card
+# This creates the appropriate card instances and gets them ready to be used in
+# the game.
+#
+# The deck can have a placeholder set for where the cards should go when they
+# are dealt; the initial properties of the created cards are duplicates of
+# these properties.
 #
 # None of these resources will be added to the tree, hence they will not
 # actually be visible anyway until they are "dealt".
+#
+# TODO: For the time being, the destination location is based on our own token.
 func _load_cards() -> void:
     if deck_cards == null:
         print("cannot load cards; no deck defined")
@@ -49,14 +55,32 @@ func _load_cards() -> void:
 
     var card := deck_cards.card_scene
     for item in deck_cards.cards:
-        print("Adding card %s" % item.token.name)
-        #var new_card = card.instantiate()
-        #new_card.token_details = item.token
-        #new_card.card_details = item.card
-        #new_card.position = Vector2(300, 300)
-        #get_parent().add_child(new_card)
+        print("Adding card: %s" % item.token)
 
+        # Create the card and set its position.
+        var new_card = card.instantiate() as BaseCard
+        new_card.position = Vector2(position.x - 200, position.y)
 
+        # Set up the visuals for the card itself.
+        new_card.token_details = item.token
+        new_card.card_details = item.card
+
+        # Get the token that says where we should deal cards to and what other
+        # token properties exist.
+        # TODO: This should not be us; this should be some pre designated token
+        #       that represents this location.
+        var deal_token : BaseToken = self
+
+        # Set up the other token properties to match those of the deal token.
+        new_card.token_padding = deal_token.token_padding
+        new_card.token_outline_width = deal_token.token_outline_width
+        new_card.token_zoom = deal_token.token_zoom
+        new_card.scale = deal_token.scale
+
+        # Create and inject the cards; we can't add these to the tree right
+        # away; if you want to do that, you need to do
+        #     get_parent().add_child.call_deferred(new_card)
+        _cards.append(new_card)
 
 
 ## -----------------------------------------------------------------------------
